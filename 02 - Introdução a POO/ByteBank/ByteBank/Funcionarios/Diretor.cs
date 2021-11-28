8677bfe32b6f6a64ebcc37a546fb4b002d3eb420
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-11-28 07:25:33 -0300

    Classe Diretor herdando de Funcionario

--- 02 - Introdução a POO/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ 02 - Introdução a POO/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -8,10 +8,6 @@ namespace ByteBank.Funcionarios
 {
     internal class Diretor : Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
-
         public double GetBonificacao()
         {
             return Salario;
