5c672c45761aa88b0ac2a8e43f651be562111117
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-12-05 15:34:46 -0300

    Initial commit

--- 04 - Entendendo Excecoes/ByteBank/ByteBank/Program.cs
+++ 04 - Entendendo Excecoes/ByteBank/ByteBank/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _07_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(867, 86716490);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
