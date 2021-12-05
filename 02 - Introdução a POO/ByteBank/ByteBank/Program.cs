34b08100b441a5f9e7da072a7399e6eafa682661
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2021-12-05 11:30:07 -0300

    New methor "UsarSistema"

--- 02 - Introdução a POO/ByteBank/ByteBank/Program.cs
+++ 02 - Introdução a POO/ByteBank/ByteBank/Program.cs
@@ -1,9 +1,28 @@
 ﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
 
 namespace ByteBank
 {
     class Program
     {
+        public static void UsarSistema()
+        {
+            SistemaInterno sistemaInterno = new SistemaInterno();
+
+            Designer designer = new Designer("Pedro", 3000, "833.222.048-39");
+
+            Diretor diretor = new Diretor("Roberta", 5000, "159.753.398-04");
+
+            Auxiliar auxiliar = new Auxiliar("Igor", 2000, "981.198.778-53");
+
+            GerenteDeConta gerenteDeConta = new GerenteDeConta("Camila", 4000, "326.985.628-89");
+
+            sistemaInterno.Logar(designer, "123");
+            sistemaInterno.Logar(diretor, "456");
+            sistemaInterno.Logar(auxiliar, "abc");
+            sistemaInterno.Logar(gerenteDeConta, "def");
+
+        }
         public static void CalcularBonificacao()
         {
             GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
@@ -26,7 +45,9 @@ namespace ByteBank
 
         static void Main(string[] args)
         {
-            CalcularBonificacao();
+            // CalcularBonificacao();
+
+            UsarSistema();
         }
     }
 }
